9ec11d7344681420cc598e138d2ba67b74a1a4fd
luweijun <luweijun@qa-tpl.jx.diditaxi.com>
2015-08-17 13:09:07 +0800

    test file encoding change

--- test/udp_client.php
+++ test/udp_client.php
@@ -1,6 +1,21 @@
 <?php
+/*
+  +----------------------------------------------------------------------+
+  | Aha                                                                  |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.0 of the Apache license,    |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+  | If you did not receive a copy of the Apache2.0 license and are unable|
+  | to obtain it through the world-wide-web, please send a note to       |
+  | yiming_6weijun@163.com so we can mail you a copy immediately.        |
+  +----------------------------------------------------------------------+
+  | Author: Weijun Lu  <yiming_6weijun@163.com>                          |
+  +----------------------------------------------------------------------+
+*/
 $client = new swoole_client(SWOOLE_SOCK_UDP, SWOOLE_SOCK_ASYNC);
-//设置事件回调函数
+//璁剧疆浜嬩欢鍥炶皟鍑芥暟
 $client->on("connect", function($cli) {
     $arrData = array(
 		'cmd' => 'demo-server-udp',
@@ -18,5 +33,5 @@ $client->on("error", function($cli){
 $client->on("close", function($cli){
     echo "Connection close\n";
 });
-//发起网络连接
+//鍙戣捣缃戠粶杩炴帴
 $client->connect('127.0.0.1', 9603, 0.5);
